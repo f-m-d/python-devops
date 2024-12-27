1c351a26670606ddeca1d68fb3bbbe76b1666cab
Francesco Maria D'Auria <30870154+f-m-d@users.noreply.github.com>
2024-12-27 15:17:19 +0000

    Add example to trow warning in pylint

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -4,3 +4,8 @@ from random import choices
 def fruit():
     fruits = ["apple", "cherry", "strawberry"]
     return choices(fruits)[0]
+
+# Example to generate a warning in pylint:
+# assigning a variable... to itself!
+# var = 1
+# var = var
